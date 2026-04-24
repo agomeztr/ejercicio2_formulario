77149cf90430efeaf180a0df773eb5d76fae5aaa
agomeztr <agomeztr@emeal.nttdata.com>
2026-04-24 14:58:21 +0200

    start form

--- web/modules/custom/form/src/Controller/FormController.php
+++ web/modules/custom/form/src/Controller/FormController.php
@@ -1,14 +1,29 @@
 <?php
-namespace Drupal\pokedex\Controller;
+namespace Drupal\form\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Component\Utility\Xss;
 
-class PokedexController extends ControllerBase {
+class FormController extends ControllerBase {
   public function content() {
     //Create the content elements
     return [
       '#markup' => '
-        
+            <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Well never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
         ',
         '#attached' => [
             'library' => 'form/form',
